2fc829ae311cdd952ff1da77b20a53f8714eb810
Anderson Silva <andssilva.23@gmail.com>
2021-12-01 12:10:45 -0400

    feat: :rocket: cria o componente Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import '../styles/content.scss';
+
+interface RatingsProps {
+  Source: string;
+  Value: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<RatingsProps>;
+  Runtime: string;
+}
+
+interface Props {
+  selectedGenre: {
+    title: string,
+  },
+  movies: Array<MovieProps>
+}
+
+export function Content({ selectedGenre, movies }: Props) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
